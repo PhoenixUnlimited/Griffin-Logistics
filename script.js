a69382d501cfde888174b7fef0d548186f3e3bb2
Chad Smith <126908171+PhoenixUnlimited@users.noreply.github.com>
2024-05-26 22:10:18 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("p1")[0].style.fontSize = "2vw";
+document.getElementsByTagName("h1")[0].style.fontSize = "5vw";
+document.getElementsByTagName("p2")[0].style.fontSize = "2vw";
+document.getElementsByTagName("p3")[0].style.fontSize = "2vw";
